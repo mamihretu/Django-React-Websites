2e4d644e4c72b4c8ee229e280af58200dc799d7e
Michael <mamihretu@mun.ca>
2022-09-14 07:40:57 -0400

    initial django and react setup

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RoomView
+
+
+
+
+urlpatterns = [
+    path('', RoomView.as_view()),
+]
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+
+
+const App = () => {
+	return(
+		<>
+		<h1> Hellow</h1>
+		<h2>This is your father</h2>
+		</>
+		)
+
+}
+
+export default App;
